06c7ee9c73690417b8589f3f3dfc1face350abee
Ak7210 <21f1006807@ds.study.iitm.ac.in>
2025-03-01 11:04:40 +0530

    adding the positional_encoding class

--- transformer_model.py
+++ transformer_model.py
@@ -22,7 +22,8 @@ import math
 
 class input_embedding(nn.Module):
 
-    ''' This class is used to create the input embedding layer for the transformer model which is used to convert the input tokens into the embedding vectors of the same size as the model dimension 
+    ''' This class is used to create the input embedding layer for the transformer model
+         which is used to convert the input tokens into the embedding vectors of the same size as the model dimension 
     Args:
         d_model: int: the dimension of the model (default= 512) also known as the embedding size
         vocab_size: int: the size of the vocabulary (depends on the dataset)
@@ -44,7 +45,77 @@ class input_embedding(nn.Module):
         '''
 
     def forward(self, x):
-        return self.embedding(x)*math.sqrt(self.d_model)
+        '''
+        This function is used to pass the input tokens through the embedding layer to get the embedding vectors
+        Args:
+            x: tensor: the input tokens of shape (batch_size, seq_len)
+            batch_size: int: the size of the batch
+            seq_len: int: the length of each input sequence
+        Returns:
+            embedding: tensor: the embedding vectors of shape (batch_size, seq_len, d_model)
+            multiplying the embedding vectors with the square root of the d_model is prevent the gradients from vanishing or exploding
+        '''
+        return self.embedding(x)*math.sqrt(self.d_model) 
+
+# 2. Positional Encoding
+
+class positional_encoding(nn.Module):
+    
+    ''' Transformers process input tokens in parallel and lack order information. 
+        To address this, a positional encoding layer adds positional information using a positional encoding matrix, ensuring the model understands token order.
+    Args:
+        d_model: int: the dimension of the model (default= 512) also known as the embedding size
+        max_len: int: the maximum length of the input sequence (default= 512)
+
+    Returns:
+        positional_encoding: tensor: the positional encoding layer for the transformer model
+
+    Note: the positional encoding is added to the input embedding vectors to add the positional information to the input tokens
+    ''''
+    def __init__(self, d_model: int, max_len: int = 512, dropout: float):
+        super(positional_encoding, self).__init__()
+        self.d_model = d_model
+        self.max_len = max_len
+        self.dropout = nn.Dropout(dropout) # adding the dropout layer to the positional encoding to prevent overfitting
+        self.positional_encoding = self.get_positional_encoding() # getting the positional encoding matrix
+
+
+
+    def get_positional_encoding(self):
+        '''
+        This function is used to create the positional encoding matrix
+        Returns:
+            positional_encoding: tensor: the positional encoding matrix of shape (max_len, d_model)
+        '''
+        positional_encoding = torch.zeros(self.max_len, self.d_model) # creating a matrix of zeros of shape (max_len, d_model)
+
+        position = torch.arange(0, self.max_len, dtype= torch.float).unsqueeze(1) # creating a position matrix of shape (max_len, 1) 
+        div_term = torch.exp(torch.arange(0, self.d_model, 2).float()*(-math.log(10000.0)/self.d_model)) # creating a division term
+
+        positional_encoding[:, 0::2] = np.sin(position*div_term) # adding the sin values to the even indices
+        positional_encoding[:, 1::2] = np.cos(position*div_term) # adding the cos values to the odd indices
+
+        positional_encoding = positional_encoding.unsqueeze(0)
+        return positional_encoding
+
+    def forward(self, x):
+        '''
+        This function is used to add the positional encoding to the input embedding vectors
+        Args:
+            x: tensor: the input embedding vectors of shape (batch_size, seq_len, d_model)
+            batch_size: int: the size of the batch
+            seq_len: int: the length of each input sequence
+        Returns:
+            x + positional_encoding: tensor: the input embedding vectors with the positional encoding added of shape (batch_size, seq_len, d_model)
+        '''
+        x = x + self.positional_encoding[:, :x.shape[1], :].requires_grad_(False) #requires_grad_(False) is used to prevent the positional encoding matrix from being updated during the training
+        x = self.dropout(x)
+        return x
+
+   
+
+
+
         
 
         
